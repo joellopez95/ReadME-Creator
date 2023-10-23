ed037ae6a159593104cedca8e542a02bcf684348
Joel Lopez <jolopez.satx@gmail.com>
2023-10-23 00:20:56 -0500

    added pakages code in js and write the kinds of questions. Worked on function write to file.

--- index.js
+++ index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//using similar approach as activity 20
+//For readME file, include:
+//title, brief description, table of contents, installation, usage, license, contributing, tests, and questions.
+
+const questions = [
+ {
+   type: '', 
+   name: '',
+   message: '', 
+ }, 
+ {
+    type: '', 
+    name: '',
+    message: '', 
+  },
+  {
+    type: '', 
+    name: '',
+    message: '', 
+  },
+  {
+    type: '', 
+    name: '',
+    message: '', 
+  },
+  {
+    type: '', 
+    name: '',
+    message: '', 
+  },
+  {
+    type: '', 
+    name: '',
+    message: '', 
+  },
+  {
+    type: '', 
+    name: '',
+    message: '', 
+  },
+  {
+    type: '', 
+    name: '',
+    message: '', 
+  },
+  {
+    type: '', 
+    name: '',
+    message: '', 
+  },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.write(fileName, data, function(err) => {
+        if (err) {
+            console.error('Error', err);
+        } else {
+            console.log(`Success`);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
