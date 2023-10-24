d2839d1f46b733ae4031fd8983541fc2fda8fae5
Joel Lopez <jolopez.satx@gmail.com>
2023-10-24 09:38:43 -0500

    removed commas and added semi colons instead for renderLicensing

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  return '',
+  return '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  return '',
+  return '';
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return '',
+  return '';
 }
 
 // TODO: Create a function to generate markdown for README
